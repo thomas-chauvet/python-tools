0e1dc3bec3e5fe6be6de998175329f0a5792dde1
Thomas Chauvet <thomas.chauvet@bleckwen.ai>
2019-02-22 11:46:13 +0100

    xgboost classifier tuning with xgboost

---
+++ src/xgboost_tuning_with_hyperopt.py
@@ -0,0 +1,295 @@
+# -*- coding: utf-8 -*-
+
+# From: https://www.kaggle.com/yassinealouini/hyperopt-the-xgboost-model
+import logging
+from sklearn import metrics
+from sklearn.metrics import classification_report
+import xgboost as xgb
+from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scikitplot as skplt
+random_state = 777
+np.random.seed(random_state)
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import ShuffleSplit
+from tqdm import tqdm
+
+
+class XGBClassifierTuning():
+    """
+    Prototype a fast tuned xgboost with hyperopt.
+    We try to find the best set of xgboost's hyperparameters that optmizes the 
+    [metrics.average_precision_score](https://scikit-learn.org/stable/modules/generated/sklearn.metrics.metrics.average_precision_score.html) 
+    per default. You can change the optimized metric with the parameter `optmized_metric`.
+
+    XGBClassifierTuning contains an helper to create a mini-report of classification performance 
+    (confusion matrix, classification report with precision, recall and f1-score, and proability distribution for each class).
+
+    :param X_train: The training input samples.
+    :param y_train: The target values for training set.
+    :param X_val: The validation input samples.
+    :param y_val: The target values for validation set.
+    :param eval_metric: evaluation metrics for validation data, a default metric will be assigned according to objective. 
+    Must be an [eval_metric](https://xgboost.readthedocs.io/en/latest/parameter.html) from xgboost.
+    :param optmized_metric: should be a [sklearn metric](https://scikit-learn.org/stable/modules/model_evaluation.html#classification-metrics) 
+    returning a number (ex: accuracy_score, metrics.average_precision_score, roc_auc_score, etc).
+    :param max_evals: try max_evals parameters combination for evaluation before returning best hyerparameters combination.
+    :param weight_imbalanced: 
+    :param verbose: if `True` xgboost is verbose.
+    :param random_state: seed used by the random number generator for reproductibility.
+    """
+
+    def __init__(self,
+                 X_train,
+                 y_train,
+                 X_val,
+                 y_val,
+                 eval_metric="auc",
+                 optmized_metric=metrics.average_precision_score,
+                 max_evals=10,
+                 weight_imbalanced=False,
+                 verbose=False,
+                 random_state=777):
+
+        self.logger = logging.getLogger(__name__)
+        self.weight_imbalanced = weight_imbalanced
+        self.max_evals = max_evals
+        self.verbose = verbose
+        self.random_state = random_state
+        self.best_hyperparams = None
+        cores_number = np.min([8, os.cpu_count()])
+        self.logger.info("Use " + str(cores_number) + " cores on " +
+                         str(os.cpu_count()) + ".")
+
+        if self.weight_imbalanced:
+            scale_pos_weight = np.min([
+                9, np.sum(1 - y_train) / np.sum(y_train)
+            ])  # https://github.com/dmlc/xgboost/issues/2428
+        else:
+            scale_pos_weight = 1
+
+        self.fix_hyperparameters = {
+            'eval_metric': eval_metric,
+            'objective': 'binary:logistic',
+            'booster': 'gbtree',
+            'tree_method': 'exact',
+            'silent': not self.verbose,
+            'seed': self.random_state,
+            'scale_pos_weight': scale_pos_weight,
+            'max_delta_step': 1,
+            'nthread': 0  #cores_number
+        }
+        self.fit_with_best_parameters(X_train, y_train, X_val, y_val)
+
+    def fit_with_best_parameters(self, X_train, y_train, X_val, y_val):
+        self.logger.info("Fit with best parameters")
+        if self.best_hyperparams is None:
+            self.optimize(X_train, y_train, X_val, y_val)
+        model = xgb.XGBClassifier(**self.best_hyperparams)
+        model.fit(X_train, y_train, verbose=self.verbose)
+        self.model = model
+
+    def __get_best_hyperparameters(self, best_hyperparams):
+        hyperparams = dict(self.fix_hyperparameters, **best_hyperparams)
+        hyperparams['n_estimators'] = int(hyperparams['n_estimators'])
+        return hyperparams
+
+    def optimize(self, X_train, y_train, X_val, y_val):
+        """
+        This is the optimization function that given a space (space here) of 
+        hyperparameters and a scoring function (score here), finds the best hyperparameters.
+        """
+
+        # To learn more about XGBoost parameters, head to this page:
+        # https://github.com/dmlc/xgboost/blob/master/doc/parameter.md
+
+        def objective_function(params):
+
+            params['n_estimators'] = int(params['n_estimators'])
+
+            self.logger.info("Training with params: ")
+            self.logger.info(params)
+
+            watchlist = [(X_train, y_train), (X_val, y_val)]
+
+            model = xgb.XGBClassifier(**params)
+
+            model.fit(
+                X_train,
+                y_train,
+                eval_set=watchlist,
+                verbose=self.verbose,
+                early_stopping_rounds=100)
+
+            predictions = model.predict_proba(
+                X_val, ntree_limit=model.best_iteration + 1)[:, 1]
+
+            valid_score = metrics.average_precision_score(y_val, predictions)
+
+            self.logger.info("\tScore {0}\n\n".format(valid_score))
+            self.logger.info("-------- End Iteration --------")
+
+            # The score function should return the loss (1-score)
+            # since the optimize function looks for the minimum
+            loss = 1 - valid_score
+
+            return {'loss': loss, 'status': STATUS_OK}
+
+        space = {
+            'n_estimators': hp.quniform('n_estimators', 100, 1000, 1),
+            'eta': hp.quniform('eta', 0.025, 0.5, 0.025),
+            # A problem with max_depth casted to float instead of int with
+            # the hp.quniform method.
+            'max_depth': hp.choice('max_depth', np.arange(1, 14, dtype=int)),
+            'min_child_weight': hp.quniform('min_child_weight', 1, 6, 1),
+            'subsample': hp.quniform('subsample', 0.5, 1, 0.05),
+            'gamma': hp.quniform('gamma', 0.5, 1, 0.05),
+            'colsample_bytree': hp.quniform('colsample_bytree', 0.5, 1, 0.05)
+        }
+        # Use the fmin function from Hyperopt to find the best hyperparameters
+        best_hyperparams = self.__get_best_hyperparameters(
+            fmin(
+                objective_function,
+                dict(self.fix_hyperparameters, **space),
+                algo=tpe.suggest,
+                max_evals=self.max_evals))
+        self.best_hyperparams = best_hyperparams
+        self.logger.info("Best parameters are:\n")
+        self.logger.info(best_hyperparams)
+
+    def report(self, X, y):
+        """
+        Helper to create a mini-report of classification performance (confusion matrix, 
+        classification report with precision, recall and f1-score, and proability distribution for each class).
+        :param X: features matrix (in general from test set).
+        :param y: target vector (in general from test set)
+        """
+        pred = self.model.predict(X)
+        skplt.estimators.plot_feature_importances(
+            self.model, x_tick_rotation=90)
+        plt.show()
+        skplt.metrics.plot_confusion_matrix(y, pred, normalize=True)
+        plt.show()
+        print("Classification report")
+        print(classification_report(y, pred))
+        skplt.metrics.plot_precision_recall(y, self.model.predict_proba(X))
+        plt.show()
+        pd.DataFrame({
+            "predict_probability": self.model.predict_proba(X)[:, 1],
+            "observed": y
+        }).hist(
+            "predict_probability", by="observed")
+        plt.suptitle("Probability distribution by class")
+        plt.show()
+
+
+def make_model(X,
+               y,
+               test_size=0.25,
+               eval_metric="auc",
+               optmized_metric=metrics.average_precision_score,
+               max_evals=10,
+               weight_imbalanced=False,
+               verbose=False,
+               random_state=777):
+    """
+    Quickly create a tuned xgboost model.
+    If categorical feature in dataset, each categorical feature is transformed with labelEncoder (from scikit-learn),
+    then we transformed data with one-hot-encoding.
+    :param X: whole feature set (matrix pandas DataFrame)
+    :param y: target vector (vector pandas Series)
+    :param test_size: size of the test set (this value is reused to split the (1-test_size) train set in train and valid set).
+    :param max_evals: try max_evals parameters combination for evaluation before returning best hyerparameters combination.
+    :param random_state: seed used by the random number generator for reproductibility.
+    :return: (X_train: features from train set,
+     y_train: target from train set, 
+     X_val: feature from validation set,
+     y_val: target from validation set,
+     X_test: feature from test set,
+     y_test: target from test set,  
+     xgboost: XGBClassifierTuning with xgboost model in it)
+    """
+
+    # Split in train, validation, test
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state)
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=test_size, random_state=random_state)
+
+    # XGboost model
+    xgboost = XGBClassifierTuning(
+        X_train, y_train, X_val, y_val, max_evals=max_evals)
+
+    return X_train, y_train, X_val, y_val, X_test, y_test, xgboost
+
+
+def score_metrics(model, X, y):
+    probs = model.predict_proba(X)[:, 1]
+    pred = model.predict(X)
+    return {
+        "accuracy_score": metrics.accuracy_score(y, pred),
+        "balanced_accuracy_score": metrics.balanced_accuracy_score(y, pred),
+        "metrics.average_precision_score": metrics.average_precision_score(y, pred),
+        "f1_score": metrics.f1_score(y, pred),
+        "precision_score": metrics.precision_score(y, pred),
+        "recall_score": metrics.recall_score(y, pred),
+        "roc_auc_score": metrics.roc_auc_score(y, pred)
+    }
+
+
+def xgboost_scores(X,
+                   y,
+                   n_splits=10,
+                   test_size=0.25,
+                   eval_metric="auc",
+                   optmized_metric=metrics.average_precision_score,
+                   max_evals=10,
+                   weight_imbalanced=False,
+                   verbose=False,
+                   random_state=777):
+    """
+    Quickly run benchmark multiple times on a dataset to evalute xgboost model on it.
+    At each iteration, we split the dataset in train / valid / test and trained a tuned xgboost model.
+    We return different classification performance metrics.
+
+    If categorical feature in dataset, each categorical feature is transformed with labelEncoder (from scikit-learn),
+    then we transformed data with one-hot-encoding.
+    :param X: whole feature set (matrix pandas DataFrame)
+    :param y: target vector (vector pandas Series)
+    :param n_splits: number of iteration to repeat.
+    :param test_size: size of the test set (this value is reused to split the (1-test_size) train set in train and valid set).
+    :param max_evals: try max_evals parameters combination for evaluation before returning best hyerparameters combination.
+    :param random_state: seed used by the random number generator for reproductibility.
+    :return: classification performance metrics for each iteration:
+     f1_score, f2_score, precision_score, recall_score, metrics.average_precision_score, roc_auc_score.
+    """
+
+    metrics = []
+    rs = ShuffleSplit(
+        n_splits=n_splits, test_size=test_size, random_state=random_state)
+
+    for train_index, test_index in tqdm(rs.split(X), total=n_splits):
+
+        # get train and test set
+        X_train = X.iloc[train_index]
+        X_test = X.iloc[test_index]
+        y_train = y.iloc[train_index]
+        y_test = y.iloc[test_index]
+
+        # Add valid set
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train, y_train, test_size=test_size, random_state=random_state)
+
+        # XGboost model
+        xgboost = XGBClassifierTuning(
+            X_train, y_train, X_val, y_val, max_evals=max_evals)
+        metrics.append(score_metrics(xgboost.model, X_test, y_test))
+
+    return metrics
+    
